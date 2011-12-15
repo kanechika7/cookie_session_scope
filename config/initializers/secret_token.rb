125072fbd5a9b824a3ac285d37d4732d1d71e4b4
kanechika <kanechika7@gmail.com>
2011-12-15 21:35:34 +0900

    cookie_session_scope start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CookieSessionScope::Application.config.secret_token = 'ccded70702987891cfc780ae1de9088bcd48218d328eb5933254cbc1a16f962ab3118267062a619cb6ef0ab782153c42ea8e7a95715fc377ae3c69715f768bd2'
